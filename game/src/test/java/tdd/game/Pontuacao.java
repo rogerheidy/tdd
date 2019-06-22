f2e99486dc206e5232378686d1007bc97f9b9d5a
Roger <rogerheidy@hotmail.com>
2019-06-22 20:17:49 -0300

    erro

--- game/src/test/java/tdd/game/Pontuacao.java
+++ game/src/test/java/tdd/game/Pontuacao.java
@@ -1,9 +1,13 @@
 package tdd.game;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Pontuacao {
-	Ponto ponto;
+	private Map<String, Integer> ponto = new LinkedHashMap<>();
+	
 	public Integer getPontos(String tipoPonto) {
-		return ponto.getQuantidade();
+		return ponto.getOrDefault(tipoPonto, 0);
 	}
 	public void adicionarPontos(String tipoPontuacao, int quantidadePontuacao) {
 		Integer pontuacaoAtual = this.getPontos(tipoPontuacao);
